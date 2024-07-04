1bd414a650967175831683b949b77229fb7b462c
PiotrBa <piotr.balazyk@wp.pl>
2024-07-04 19:31:51 +0100

    My first commit

---
+++ src/test/java/com/piotrba/spring_docker_aws/SpringDockerAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.piotrba.spring_docker_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
